b37dda5b3d5e8f2acba0aa3a609b15d466c11c55
FIRST_NAME LAST_NAMEErikas <skardziuserikas@gmail.com>
2024-03-25 15:47:58 +0200

    Set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM  from 'react-dom/client'
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { CardsProvider } from './contexts/CardsContext';
-import { UsersProvider } from './contexts/UsersContext';
+import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <CardsProvider>
-      <UsersProvider>
-        <App />
-      </UsersProvider>
-    </CardsProvider>
-  </BrowserRouter>
+
+<BrowserRouter>
+<App />
+</BrowserRouter>
+
 );
