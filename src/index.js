41f517071fd3b7127e890143716af772ee92d700
FIRST_NAME LAST_NAMEErikas <skardziuserikas@gmail.com>
2024-03-25 13:03:10 +0200

    Darbo pagrindine struktura

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { CardsProvider } from './contexts/CardsContext';
+import { UsersProvider } from './contexts/UsersContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <CardsProvider>
+      <UsersProvider>
+        <App />
+      </UsersProvider>
+    </CardsProvider>
+  </BrowserRouter>
+);
